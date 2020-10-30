612039872bd8cc7d6d67359df4205d4fb8a1ebac
Ben8842 <z87e99b44r55a@gmail.com>
2020-10-30 10:47:42 -0400

    first

---
+++ src/store/reducers/movielistReducer.js
@@ -0,0 +1,16 @@
+import {UPDATE_MOVIES} from '../actions/updatemovies';
+
+const movielistReducer = (state ={}, {type, payload}) => {
+
+    switch(type) {
+
+        case UPDATE_MOVIES :
+            return {name: payload}
+            default :
+            return state
+        };
+    };
+
+    
+export default movielistReducer;
+
